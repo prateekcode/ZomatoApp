4246a1e0a394db1bbc8863cbb87149ad5f6268a4
Prateek <codes.prateek@gmail.com>
2021-01-24 11:29:24 +0530

    :lock: API Key Removed

--- app/src/main/java/com/androidcodes/zomatoapp/utils/Constants.kt
+++ app/src/main/java/com/androidcodes/zomatoapp/utils/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object{
         const val BASE_URL = "https://developers.zomato.com/api/v2.1/"
-        const val API_KEY = "7b944f3854fcd30bcac7dc6fc71fee2d"
+        const val API_KEY = "ENTER YOUR API"
     }
 
 }
